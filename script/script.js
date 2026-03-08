db20a3477f654abfbb5f22d6005de721dc1d4ac4
mdmoheburrahmanmonir <mdmohiburrahmanmanik@gmail.com>
2026-03-08 09:49:33 +0600

    Login page design and functionality implementation done.

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let logInBtn = document.getElementById('logInBtn');
+
+logInBtn.addEventListener('click',()=>{
+    let emailBox = document.getElementById('emailBox').value;
+    let PassBox = document.getElementById('PassBox').value;
+    console.log(emailBox, PassBox);
+    if (emailBox === 'admin' && PassBox === 'admin123') {
+        document.location.assign("../homePage.html")
+    }else{
+        alert('Please provide a valid Email & Pass');
+    }
+})
